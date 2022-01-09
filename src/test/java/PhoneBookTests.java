6a768d116c958c37f334518d88a575d3a762acd7
Alex010011 <bunkof3@yandex.ru>
2022-01-10 01:51:50 +0200

    xPath_practice

--- src/test/java/PhoneBookTests.java
+++ src/test/java/PhoneBookTests.java
@@ -26,6 +26,7 @@ public class PhoneBookTests {
 
        WebElement element = wd.findElement(By.tagName("a"));
        wd.findElement(By.cssSelector("a"));
+       wd.findElement(By.xpath("//a"));
        element.click();
        element.clear();
        element.sendKeys("Hello");
@@ -40,18 +41,41 @@ public class PhoneBookTests {
        WebElement elClass = wd.findElement(By.className("container"));
        wd.findElement(By.cssSelector(".container"));
 
-       wd.findElement(By.xpath("//*[@class='container'"));
+       wd.findElement(By.xpath("//*[@class='container']"));
 
        WebElement elName = wd.findElement(By.name("NameEx"));
+       wd.findElement(By.cssSelector("[name='nameEx']"));
        wd.findElement(By.xpath("//*[@name = 'nameEx']"));
-       wd.findElement(By.xpath("[name ='nameEx']"));
-
 
        WebElement elLink = wd.findElement(By.linkText("HOME"));
        WebElement elPlink = wd.findElement(By.partialLinkText("HOM"));
+
        wd.findElement(By.cssSelector("[href='/home']"));
+       wd.findElement(By.xpath("//*[@href = '/home']"));
        wd.findElement(By.cssSelector("a[href='/home'"));
-       wd.findElement(By.xpath("//*[@href ='/home']"));
+       wd.findElement(By.xpath("//a[@href ='/home']"));
+
+       wd.findElement(By.xpath("//*[text()='PHONEBOOK']"));
+       wd.findElement(By.xpath("//h1[text()='PHONEBOOK']"));
+       wd.findElement(By.xpath("//a[text()='HOME']"));
+       wd.findElement(By.xpath("//*[text()=' Login']"));
+       wd.findElement(By.xpath("//button[text()=' Login']"));
+       wd.findElement(By.xpath("//*[@placeholder='Password']"));
+       wd.findElement(By.xpath("//*[contains(@placeholder,'word')]"));
+       wd.findElement(By.xpath("//*[starts-with(@placeholder,'Pass')]"));
+       wd.findElement(By.xpath("//*[@id='root' and @class='container')]"));
+       wd.findElement(By.xpath("//*[@id='root' or @class='container')]"));
+        wd.findElement(By.xpath("//*[@class='container')][2]"));
+
+        //  xPath: //button[last()] - find last element from many, //button[1] - find first element
+        wd.findElements(By.xpath("//h1/ancestor::*"));//returns all parents
+        wd.findElement(By.xpath("//h1/ancestor::div[1]"));//
+        wd.findElements(By.xpath("//h1/ancestor-or-self::*"));//returns all parents himself
+        wd.findElement(By.xpath("//input[1]/following-sibling::*"));//returns children of the same floor down
+        wd.findElement(By.xpath("//input[1]/following-sibling::button[2]"));
+        wd.findElement(By.xpath("//input[1]/preceding-sibling::*"));//returns children of the same floor up
+        wd.findElement(By.xpath("//input[1]/preceding-sibling::input[1]"));
+        wd.findElement(By.xpath("//button[2]/parent::*"));//returns parent
 
     }
 
@@ -76,6 +100,41 @@ public class PhoneBookTests {
 
         WebElement buttonLogin = wd.findElement(By.tagName("button"));
         buttonLogin.click();
+    }
+
+    @Test
+    public void loginCss(){
+
+        click(By.cssSelector("[placeholder='Email']"));
+        type(By.cssSelector("[placeholder='Email']"),"noa@gmail.com");
+
+        click(By.cssSelector("[placeholder='Password']"));
+        type(By.cssSelector("[placeholder='Password']"), "Nnoa12345$");
+
+        click(By.cssSelector("div button:first-of-type"));
+    }
+
+    @Test
+    public void loginXPath(){
+
+        click(By.xpath("//*[@placeholder='Email']"));
+        type(By.xpath("//*[@placeholder='Email']"), "noa@gmail.com");
+
+        click(By.xpath("//*[@placeholder='Password']"));
+        type(By.xpath("//*[@placeholder='Password']"), "Nnoa12345$");
+
+        click(By.xpath("//*[text()=' Login']"));
+
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
     }
 
     @Test
--- src/test/java/TableCss.java
+++ src/test/java/TableCss.java
@@ -22,27 +22,37 @@ public class TableCss {
 
         //find element
         wd.findElement(By.cssSelector("table"));
+        wd.findElement(By.xpath("//table"));
 
         // tr9
-        wd.findElement(By.cssSelector("tr:nth-child(9)"));
+        wd.findElement(By.cssSelector("tr:last-child"));
+        wd.findElement(By.xpath("//tr[9]"));
 
         //Maria Anders
         wd.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(2)"));
-        //last column
-      //  wd.findElements(By.cssSelector(""));
+        wd.findElement(By.xpath("//tr[2]/td[2]"));
+        wd.findElement(By.xpath("//td[text()='Maria Anders']"));
 
+        //last column
+
+        wd.findElements(By.cssSelector("td:last-child"));
+        wd.findElements(By.xpath("//td[last()]"));
 
         //th elements
-       // wd.findElements(By.cssSelector(""));
+        wd.findElements(By.cssSelector("th"));
+        wd.findElements(By.xpath("//th"));
 
         //Centro comercial Moctezuma Francisco Chang Mexico - one element
-       // wd.findElement(By.cssSelector(""));
+        wd.findElement(By.cssSelector("tr:nth-child(4)"));
+        wd.findElement(By.xpath("//tr[4]"));
 
         //Giovanni Rovelli
-      //  wd.findElement(By.cssSelector(""));
+        wd.findElement(By.cssSelector("tr:last-child td:nth-child(2)"));
+        wd.findElement(By.xpath("//tr[last()]/td[2]"));
 
         //first column
-      //  wd.findElements(By.cssSelector(""));
+         wd.findElements(By.cssSelector("td:first-child"));
+         wd.findElements(By.xpath("//td[1]"));
 
     }
 }
